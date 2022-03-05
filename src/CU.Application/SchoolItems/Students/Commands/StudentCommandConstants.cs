45137dd8f1abf07f59a7f274cb839ed4db000317
Bob Goodearl <bg.gitguy@rgoodsoftware.com>
2022-03-05 11:21:54 -0500

    StudentCommandConstants - needed by prior change

---
+++ src/CU.Application/SchoolItems/Students/Commands/StudentCommandConstants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CU.Application.SchoolItems.Students.Commands
+{
+    internal class StudentCommandConstants
+    {
+        internal static DateTime Jan1_1900 = new DateTime(1900, 1, 1);
+        internal static DateTime Jan1_2300 = new DateTime(2300, 1, 1);
+    }
+}
