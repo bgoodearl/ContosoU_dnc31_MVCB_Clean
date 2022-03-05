00a42a65856d9c58afbe2b6ea15650074478f5f8
Bob Goodearl <bg.gitguy@rgoodsoftware.com>
2022-03-05 10:23:33 -0500

    Added StudentEditDto (needed with prior changes)

---
+++ src/CU.Application.Shared/ViewModels/Students/StudentEditDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CU.Application.Shared.ViewModels.Students
+{
+    public class StudentEditDto
+    {
+        public int ID { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Display(Name = "First Name")]
+        public string FirstMidName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Last name cannot be longer than 50 characters.")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+    }
+}
